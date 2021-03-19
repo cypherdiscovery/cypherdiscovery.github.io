a8d1225224a0de0c0a8290cd3ab45183594051f0
Cypher-discovery <55668912+cypherdiscovery@users.noreply.github.com>
2021-03-19 22:39:48 +0000

    Add files via upload

--- javascript/caesarAffine.js
+++ javascript/caesarAffine.js
@@ -1,22 +1,77 @@
+
+function Known(){
+	document.querySelector('input[name="key"]:checked').checked=false;
+	var knownKey = document.getElementById("quantity").value;
+	document.getElementById("keybox1").innerHTML = "<label class='container'><span id='key1'>"+knownKey+"<span><input type='radio' checked='checked' name='key' value="+knownKey+" onclick='UpdateCaesar()'><span class='checkmark'></span></label>";	
+	document.getElementById("keybox2").style.display = "none";
+	document.getElementById("keybox3").style.display = "none";
+	document.getElementById("keybox4").style.display = "none";
+	document.getElementById("keybox5").style.display = "none";
+}
+function Random(){
+	var keys = [1,2,3,4,5];
+	for (i = 0; i < 5; i++){
+		var random=Math.floor(Math.random() * 25)+1;
+		
+		var present = true;
+		while (present)
+		{
+			present = false;
+			for (n = 0; n < 5; n++){
+				if(keys[n]==random){
+					present=true;
+				}
+			}
+			if(present){
+				random=Math.floor(Math.random() * 25)+1;
+			}else{
+				keys[i]=random;
+			}
+		}
+	}
+	var ordered = false;
+	while(!ordered){
+		ordered=true;
+		for(i = 0; i < 5; i++){
+			if(keys[i]>keys[i+1]){
+				var temp = keys[i+1];
+				keys[i+1]=keys[i];
+				keys[i]=temp;
+				ordered = false;
+			}
+		}
+	}
+	document.getElementById("keybox2").style.display = "block";
+	document.getElementById("keybox3").style.display = "block";
+	document.getElementById("keybox4").style.display = "block";
+	document.getElementById("keybox5").style.display = "block";
+	
+	document.getElementById("keybox1").innerHTML = "<label class='container'><span id='key1'>"+keys[0]+"<span><input type='radio' checked='checked' name='key'  value="+keys[0]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";	
+	document.getElementById("keybox2").innerHTML = "<label class='container'><span id='key2'>"+keys[1]+"<span><input type='radio' name='key' value="+keys[1]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
+	document.getElementById("keybox3").innerHTML = "<label class='container'><span id='key3'>"+keys[2]+"<span><input type='radio' name='key' value="+keys[2]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
+	document.getElementById("keybox4").innerHTML = "<label class='container'><span id='key4'>"+keys[3]+"<span><input type='radio' name='key' value="+keys[3]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
+	document.getElementById("keybox5").innerHTML = "<label class='container'><span id='key5'>"+keys[4]+"<span><input type='radio' name='key' value="+keys[4]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
+}
+
 function Cypher(a, b){
 	console.log("cypher");
 	var letterArray = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
-	var input = document.getElementById("input").textContent;
+	var input = document.getElementById("input").value;
 	var cleanedInput = input.toUpperCase();
 	var output = "";
-	if(document.getElementById("input").childNodes.length==0){
+	if(document.getElementById("input").value==""){
 		cleanedInput="ATTACK AT DAWN...";
 	}
 	for(i = 0; i<cleanedInput.length; i++){
 		var found = false;
 		for(s = 0; s<26; s++){
 			if(cleanedInput.charAt(i)==letterArray[s]){
-				number = (s+1);
+				number = (s);
 				found = true;
 			}
 		}
 		if(found){
-			var newNumber=((a*number + b)%26)-1;
+			var newNumber=((a*number + b)%26);
 			console.log(number,  newNumber, a, b);
 			output=output.concat(letterArray[newNumber]);
 		}else{
@@ -28,12 +83,12 @@ function Cypher(a, b){
 	}
 	
 }
-function CaesarDecypher(a, b){
+function Decypher(a, b){
 	console.log("decypher "+a+" "+b);
 	var letterArray = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 	var aNumbers = [1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25];
 	var modCoefficients = [1, 9, 21, 15, 3, 19, 7, 23, 11, 5, 17, 25];
-	var input = document.getElementById("input").textContent.toUpperCase();
+	var input = document.getElementById("input").value.toUpperCase();
 	var output = "";
 	var modInverse = 1;
 	for(i = 0; i<26; i++){
@@ -42,7 +97,7 @@ function CaesarDecypher(a, b){
 		}
 	}
 	console.log("mod inverse "+modInverse);
-	if(document.getElementById("input").childNodes.length==0){
+	if(document.getElementById("input").value==""){
 		input="ATTACK AT DAWN...";
 	}
 	for(i = 0; i<input.length; i++){
@@ -112,19 +167,19 @@ function solveCaesarKey(){
 			likely[i]+=26;
 		}
 	}
-	document.getElementById("keybox1").innerHTML = "<label class='container'><span id='key1'>"+likely[0]+"<span><input type='radio' checked='checked' name='key'  value="+likely[0]+" onclick='UpdateCaesar()'><span class='checkmark'></span></label>";	
-	document.getElementById("keybox2").innerHTML = "<label class='container'><span id='key2'>"+likely[1]+"<span><input type='radio' name='key' value="+likely[1]+" onclick='UpdateCaesar()'><span class='checkmark'></span></label>";
-	document.getElementById("keybox3").innerHTML = "<label class='container'><span id='key3'>"+likely[2]+"<span><input type='radio' name='key' value="+likely[2]+" onclick='UpdateCaesar()'><span class='checkmark'></span></label>";
-	document.getElementById("keybox4").innerHTML = "<label class='container'><span id='key4'>"+likely[3]+"<span><input type='radio' name='key' value="+likely[3]+" onclick='UpdateCaesar()'><span class='checkmark'></span></label>";
-	document.getElementById("keybox5").innerHTML = "<label class='container'><span id='key5'>"+likely[4]+"<span><input type='radio' name='key' value="+likely[4]+" onclick='UpdateCaesar()'><span class='checkmark'></span></label>";
+	document.getElementById("keybox1").innerHTML = "<label class='container'><span id='key1'>"+likely[0]+"<span><input type='radio' checked='checked' name='key'  value="+likely[0]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";	
+	document.getElementById("keybox2").innerHTML = "<label class='container'><span id='key2'>"+likely[1]+"<span><input type='radio' name='key' value="+likely[1]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
+	document.getElementById("keybox3").innerHTML = "<label class='container'><span id='key3'>"+likely[2]+"<span><input type='radio' name='key' value="+likely[2]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
+	document.getElementById("keybox4").innerHTML = "<label class='container'><span id='key4'>"+likely[3]+"<span><input type='radio' name='key' value="+likely[3]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
+	document.getElementById("keybox5").innerHTML = "<label class='container'><span id='key5'>"+likely[4]+"<span><input type='radio' name='key' value="+likely[4]+" onclick='UpdateCaesar(\"checkbox\")'><span class='checkmark'></span></label>";
 
 }
 function frequencyAnalasis(){
 	console.log("frequency analasis");
 	var letterArray = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
 	var freqArray = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
-	var input = document.getElementById("input").textContent.toUpperCase();
-	if(document.getElementById("input").childNodes.length==0){
+	var input = document.getElementById("input").value.toUpperCase();
+	if(document.getElementById("input").value==""){
 		input="ATTACK AT DAWN...";
 	}
 	for (i = 0; i<input.length; i++){
@@ -137,9 +192,17 @@ function frequencyAnalasis(){
 	}
 	return freqArray;
 }
-function UpdateCaesar(){
-	if(document.getElementById("Random").checked && document.getElementById("decypher").checked){
+function UpdateCaesar(type){
+	if(type=="textarea"){
+		if(document.getElementById("Random").checked && document.getElementById("decypher").checked && type=="textarea"){
 		solveCaesarKey();
+		}
+	}else{
+		if(document.getElementById("Random").checked && document.getElementById("cypher").checked){
+			Random();
+		}else if(document.getElementById("Known").checked){
+			Known();
+		}
 	}
 	var key = document.querySelector('input[name="key"]:checked').value;
 	console.log("key " + key);
@@ -147,6 +210,6 @@ function UpdateCaesar(){
 	if(document.getElementById("cypher").checked){
 		Cypher(1, b);
 	}else{
-		CaesarDecypher(1, b);
+		Decypher(1, b);
 	}
 }
